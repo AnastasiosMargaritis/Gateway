2fb2331694badf503f7b82ddf64da89ac835e416
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-12-06 15:17:16 +0200

    Implementing Basic OAuth2.0 Security.

--- Spirng Cloud/SecurityJWT/src/main/java/com/security/security/JWTOAuth2Config.java
+++ Spirng Cloud/SecurityJWT/src/main/java/com/security/security/JWTOAuth2Config.java
@@ -3,10 +3,17 @@ package com.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
@@ -15,8 +22,12 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
+@EnableOAuth2Client
 public class JWTOAuth2Config  extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
@@ -53,6 +64,7 @@ public class JWTOAuth2Config  extends AuthorizationServerConfigurerAdapter {
                 .exceptionTranslator(translator);
     }
 
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
@@ -63,5 +75,7 @@ public class JWTOAuth2Config  extends AuthorizationServerConfigurerAdapter {
                         "password",
                         "client_credentials"
                 ).scopes("webclient", "mobileclient");
+
+
     }
 }
---
+++ Spring Cloud OAuth2/OAuth2/src/main/java/com/oauth2/OAuth2Application.java
@@ -0,0 +1,41 @@
+package com.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class OAuth2Application extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public String message(@AuthenticationPrincipal OAuth2User principal){
+		return "Hi " + principal.getName() + " you are authenticated via Github.";
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/Intranet/Bokning").authenticated() // Block this
+				.antMatchers("/**", "/Intranet**").permitAll() // Allow this for all
+				.anyRequest().authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll()
+				.and()
+				.oauth2Login();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
+
+}
